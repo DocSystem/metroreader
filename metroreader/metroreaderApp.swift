00e4380007ae16c524a021eb7603887650164fa8
DocSystem <42613513+DocSystem@users.noreply.github.com>
2024-12-31 15:31:25 +0100

    Initial Commit

---
+++ metroreader/metroreaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  metroreaderApp.swift
+//  metroreader
+//
+//  Created by Antoine Souben-Fink on 31/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct metroreaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
